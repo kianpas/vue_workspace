7bda8aa3c6a6724d849f00b05c93f91de09bc0bd
kianpas <kianpas@gmail.com>
2021-12-07 22:18:33 +0900

    21.12.07 event, modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,16 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      confirmedName: "",
     };
   },
   methods: {
-    add(num){
+    add(num) {
       this.counter = this.counter + num;
     },
-    remove(num){
+    remove(num) {
       this.counter = this.counter - num;
+    },
+    setName(event, name) {
+      this.name = event.target.value + " " + name;
+    },
+    submitForm() {
+      alert("Submitted");
+    },
+    confirmInput(){
+      this.confirmedName = this.name;
     }
   },
 });
 
-app.mount('#events');
+app.mount("#events");
