6e55002db4cebe1ec8c52e18cb85ae612940acad
kianpas <kianpas@gmail.com>
2021-12-20 21:53:25 +0900

    21.12.20 basics05 컴퓨트

---
+++ basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,40 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+    };
+  },
+  computed:{
+    fullname(){
+      console.log("running");
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Sch";
+    }
+  },
+  methods: {
+    outputFullname() {
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Sch";
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = "";
+    },
+  },
+});
+
+app.mount("#events");
