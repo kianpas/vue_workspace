7473263f934f4039993c47b9673ae4d2523e87dd
kianpas <kianpas@gmail.com>
2022-02-28 21:34:15 +0900

    22.02.28 learning resources app first

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official vue.js doc',
+          link: 'https://vuejs.org',
+        },
+
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn the google',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
