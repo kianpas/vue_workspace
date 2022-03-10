81de68d6349043ec6016d760d873a85d74689855
kianpas <kianpas@gmail.com>
2022-03-10 20:34:15 +0900

    22.03.10 dynamic compo

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,13 +1,12 @@
 <template>
-<the-header title="RememberMe"></the-header>
+  <the-header title="RememberMe"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
-import TheHeader from "./components/layouts/TheHeader.vue"
+import TheHeader from './components/layouts/TheHeader.vue';
 export default {
-  components: { StoredResources, TheHeader },
+  components: { TheHeader },
   data() {
     return {
       storedResources: [
---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,33 @@
+<template>
+  <base-card>
+    <!-- 커스텀 컴포넌트도 이벤트 전달 가능 -->
+    <base-button @click="setSelectedTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setSelectedTab('add-resources')"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+export default {
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
