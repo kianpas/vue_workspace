34d76af5ee897efd94fb5ab18a4cadf986be9287
kianpas <kianpas@gmail.com>
2022-03-15 22:21:42 +0900

    22.03.15 modal dialog

--- prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -1,4 +1,19 @@
 <template>
+  <base-dialog
+    v-if="inputIsInvalid"
+    title="Invalid Input"
+    @close="confirmError"
+  >
+    <template #default>
+      <p>Unfortunately, at lease one input value is invalid</p>
+      <p>check input field</p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError" @close="confirmError"
+        >Okay</base-button
+      >
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -26,14 +41,32 @@
 </template>
 
 <script>
+import BaseButton from '../UI/BaseButton.vue';
 export default {
+  components: { BaseButton },
   inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
   methods: {
     submitData() {
       const enteredTitle = this.$refs.titleInput.value;
       const enteredDesc = this.$refs.descInput.value;
       const enteredUrl = this.$refs.linkInput.value;
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDesc.trim() === '' ||
+        enteredUrl.trim() === ''
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
       this.addResource(enteredTitle, enteredDesc, enteredUrl);
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
     },
   },
 };
