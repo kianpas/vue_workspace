5f3c7dffd2a78e86a43dc843a82d9bcc453d1a68
kianpas <kianpas@gmail.com>
2022-04-15 20:55:53 +0900

    22.04.15 catchall routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -5,16 +5,18 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 //props true로 props 값으로 라우팅 가능, props 방식이 재활용가능
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component: TeamsList },
+    { path: '/', redirect: '/teams' },
+    { path: '/teams', component: TeamsList }, //alias 방식은 url 변경 안됨
     { path: '/users', component: UsersList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', component: NotFound }, //정의되지않은 url 전부
   ],
-
   linkActiveClass: 'active',
 });
 
