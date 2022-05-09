0f80e5a163a592c9c648be43773c472dc543391d
kianpas <kianpas@gmail.com>
2022-05-09 21:00:57 +0900

    22.05.09 199

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -11,10 +11,10 @@
     </transition>
     <button @click="togglePara">Toggle Paragraph</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
-    <p>This is a test dialog!</p>
-    <button @click="hideDialog">Close it!</button>
-  </base-modal>
+    <base-modal @close="hideDialog" :open="dialogIsVisible">
+      <p>This is a test dialog!</p>
+      <button @click="hideDialog">Close it!</button>
+    </base-modal>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
@@ -137,4 +137,6 @@ button:active {
     transform: translateX(-150px) scale(1);
   }
 }
+
+
 </style>
--- animations-01-starting-setup/src/components/BaseModal.vue
+++ animations-01-starting-setup/src/components/BaseModal.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="backdrop" @click="$emit('close')"></div>
-  <dialog open>
-    <slot></slot>
-  </dialog>
+  <div v-if="open" class="backdrop" @click="$emit('close')"></div>
+  <transition name="modal">
+    <dialog open v-if="open">
+      <slot></slot>
+    </dialog>
+  </transition>
 </template>
 
 <script>
 export default {
+  props: ['open'],
   emits: ['close'],
 };
 </script>
@@ -34,7 +37,16 @@ dialog {
   background-color: white;
   z-index: 100;
   border: none;
-  animation: modal 0.3s ease-out forwards;
+  /* animation: modal 0.3s ease-out forwards; */
+}
+
+.modal-enter-from {
+}
+.modal-enter-active {
+  animation: modal 0.3s ease-out;
+}
+.modal-leave-active {
+  animation: modal 0.3s ease-in reverse;
 }
 
 @keyframes modal {
@@ -43,7 +55,7 @@ dialog {
     transform: translateY(-50px) scale(0.9);
   }
 
-  to{
+  to {
     opacity: 100;
     transform: translateY(0px) scale(1);
   }
