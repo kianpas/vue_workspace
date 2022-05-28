96c09e4ed13c2483f9fa0fbcbb87c0c7b52f09ef
kianpas <kianpas@gmail.com>
2022-05-29 00:43:18 +0900

    22.05.29 239

--- main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>Register</h1>
+  <h1>Detail</h1>
+  <router-view></router-view>
+  <router-link to="/coaches/c1/contact/">Contact</router-link>
 </template>
 
 <script>
--- main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
+++ main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Request</h1>
+  <h1>Requests</h1>
 </template>
 
 <script>
