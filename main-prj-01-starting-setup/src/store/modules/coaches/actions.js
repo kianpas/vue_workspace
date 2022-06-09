eef4710e6ce0020a27e734498670e9f4e7a6cce6
kianpas <kianpas@gmail.com>
2022-06-09 21:28:47 +0900

    22.06.09 254

--- main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -14,10 +14,12 @@ export default {
       { method: 'PUT', body: JSON.stringify(coachData) }
     );
 
-    // const responseData = await response.json();
+    const responseData = await response.json();
 
     if (!response.ok) {
       //error
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
     }
 
     context.commit('registerCoach', { ...coachData, id: userId });
@@ -30,6 +32,8 @@ export default {
     const responseData = await response.json();
     if (!response.ok) {
       //error
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
     }
     const coaches = [];
 
