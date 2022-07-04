640f700aea41c67fd60ddb1c07aa7d656a92098c
kianpas <kianpas@gmail.com>
2022-07-04 20:45:31 +0900

    22.07.04 296

--- composition-01-starting-setup/src/components/UserData.vue
+++ composition-01-starting-setup/src/components/UserData.vue
@@ -1,14 +1,23 @@
 <template>
-  <h2>{{ uName }}</h2>
-  <h3>{{ age }}</h3>
+  <div>
+    <h2>{{ uName }}</h2>
+    <h3>{{ age }}</h3>
+  </div>
 </template>
 
 <script setup>
-import { defineProps, computed } from 'vue';
+import { defineProps, defineEmits, computed } from 'vue';
+
 const props = defineProps({
   firstName: String,
   lastName: String,
+  age: String,
 });
+
+const emit = defineEmits(['save-data']);
+
+console.log(props);
+console.log(emit);
 
 // const { firstName, lastName } = ref(props);
 
