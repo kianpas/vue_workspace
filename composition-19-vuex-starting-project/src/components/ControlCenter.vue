438be88781f823001f3505ced75987acf46beb7f
kianpas <kianpas@gmail.com>
2022-07-14 23:33:21 +0900

    22.07.14 305

---
+++ composition-19-vuex-starting-project/src/components/ControlCenter.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="inc">Increment</button>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+export default {
+  setup() {
+    //this.$store대신
+    const store = useStore();
+    function inc() {
+      store.dispatch('increment');
+    }
+
+    return { inc };
+  },
+};
+</script>
